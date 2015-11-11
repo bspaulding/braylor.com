ac3f0af8ebcaf19c27a5c0aa12d7e93591bfd4b0
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-10 23:23:52 -0800

    update webpack config to use react, stage-1 babel plugins.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: "./src/js/index.jsx",
   output: {
     path: "./dist",
     filename: "bundle.js"
   },
   module: {
     loaders: [{
-      test: /\.jsx?/,
-      exclude: /node_modules/,
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
       loader: "babel",
       query: {
         cacheDirectory: true,
-        presets: ["es2015"]
+        presets: ["es2015", "react", "stage-1"]
       }
     }]
   }
