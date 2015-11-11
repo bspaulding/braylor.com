938935fe830cf164a47eecd7bc9f4c57e17751dd
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-11 00:34:29 -0800

    Rendering styles + bootstrap styles

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,4 +2,7 @@ import App from "./components/app.js";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "bootstrap/less/bootstrap.less";
+import "../css/braylor.css";
+
 ReactDOM.render(React.createFactory(App)(window.APP_PROPS), document.querySelector("#react-container"));
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,22 @@ module.exports = [{
         cacheDirectory: true,
         presets: ["es2015", "react", "stage-1"]
       }
+		}, {
+			test: /\.jpg/, loader: "url?mimetype=image/jpg"
+		}, {
+			test: /\.css/, loaders: ["style", "css"]
+		}, {
+			test: /\.less/, loaders: ["style", "css", "less"]
+		}, {
+			test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"
+		}, {
+			test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff2"
+		}, {
+    	test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"
+		}, {
+    	test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"
+		}, {
+    	test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"
     }]
   },
 	target: "web"
