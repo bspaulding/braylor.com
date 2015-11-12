2e699be18bc5031234389775ea24547481798fd4
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-12 00:17:48 -0800

    Updated countdown to handle less than 3 digit days, stop at 0, added opacity

--- src/js/components/countdown.jsx
+++ src/js/components/countdown.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import scribbleCircleURL from "../../images/scribble-circle.svg";
 
-let outerStyle = {
-	backgroundImage: "url(" + scribbleCircleURL + ")",
-	backgroundRepeat: "no-repeat",
-	backgroundSize: "120px 120px",
-	fontFamily: "Chalkduster",
-	fontSize: 24,
-	height: 120,
-	lineHeight: "34px",
-	paddingTop: (120 / 2 - 34),
-	textAlign: "center",
-	width: 120
+let styles = {
+	outer: {
+		backgroundImage: "url(" + scribbleCircleURL + ")",
+		backgroundRepeat: "no-repeat",
+		backgroundSize: "120px 120px",
+		fontFamily: "Chalkduster",
+		fontSize: 24,
+		height: 120,
+		lineHeight: "34px",
+		paddingTop: (120 / 2 - 34),
+		textAlign: "center",
+		width: 120
+	},
+	span: {
+		opacity: 0.8
+	}
 };
 
 class Countdown extends React.Component {
@@ -23,10 +28,10 @@ class Countdown extends React.Component {
 	static defaultProps = { style: {} };
 
 	render() {
-		let days = Math.floor((Date.parse(this.props.to) - (new Date()).getTime()) / (1000 * 60 * 60 * 24));
+		let days = Math.max(0, Math.floor((Date.parse(this.props.to) - (new Date()).getTime()) / (1000 * 60 * 60 * 24)));
 		return (
-			<div style={Object.assign({}, this.props.style, outerStyle)}>
-				{days} days
+			<div style={Object.assign({}, this.props.style, styles.outer)}>
+				<span style={styles.span}>{days}<br/>days</span>
 			</div>
 		);
 	}
