129d64661210c0e8ca220394eaba9a89427d674e
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-11 23:22:18 -0800

    Added open sans, article styles

--- src/js/components/story.jsx
+++ src/js/components/story.jsx
@@ -1,11 +1,53 @@
 import Banner from "./banner.jsx";
 import React from "react";
 
+let styles = {
+	fancyLetter: {
+		fontFamily: "Dancing Script",
+		fontSize: 38,
+		lineHeight: "42px",
+		opacity: 0.5,
+		position: "absolute",
+		top: -10,
+		left: -10,
+		zIndex: -1
+	},
+	p: {
+		fontFamily: "Open Sans, sans serif",
+		fontSize: 18,
+		lineHeight: "24px",
+		margin: "0px auto 10px",
+		maxWidth: 380,
+		position: "relative",
+		textShadow: "0px -1px 0px black"
+	}
+};
+
 class Story extends React.Component {
 	static displayName = "Story";
 
 	render() {
-		return <Banner>Story</Banner>;
+		return (
+			<div className="row">
+				<div className="col-xs-12">
+					<Banner>Story</Banner>
+					<p style={styles.p}>
+						<span style={styles.fancyLetter}>D</span>
+						Dui nisl sodales felis, ut
+						fermentum sapien nibh eu nunc.  Lorem ipsum dolor sit amet. Integer
+						sed magna. Duis nisl nulla, porta ut, molestie sit amet, tincidunt
+						eu, enim. Cras eu mauris. Cras iaculis, nisi vel tempor fringilla,
+						nisl dolor imperdiet dolor, id lobortis ligula nunc sed dolor.  Class
+						aptent taciti sociosqu ad litora torquent per conubia nostra, per
+						inceptos himenaeos. Curabitur eu dui vitae nulla tempor consectetuer.
+						Suspendisse ligula dolor, varius nec, vulputate id, luctus sed,
+						lacus. Pellentesque purus urna, porta molestie, mattis non, posuere
+						et, velit. Curabitur diam mauris, dictum vel, lacinia congue,
+						molestie at, nisi. Proin tempus diam ut ligula.
+					</p>
+				</div>
+			</div>
+		);
 	}
 }
 
