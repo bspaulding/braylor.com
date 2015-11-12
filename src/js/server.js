ea6c1f8eb09001126764da059cc57df3635c79a4
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-12 10:54:07 -0800

    Server uses cluster api

--- src/js/server.js
+++ src/js/server.js
@@ -1,15 +1,28 @@
 import fs from "fs";
+import cluster from "cluster";
 import http from "http";
+import { cpus } from "os";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
 import { match, RoutingContext } from 'react-router'
 import routes from "./components/routes.jsx";
 import { parse } from "url";
 
+if (cluster.isMaster) {
+	for (var i = 0; i < cpus().length; i += 1) {
+		cluster.fork();
+	}
+
+	cluster.on('exit', function(worker, code, signal) {
+		console.log('worker ' + worker.process.pid + ' died');
+	});
+} else {
+
 var template = fs.readFileSync("./dist/index.html")
 	.toString();
 
 var server = http.createServer((request, response) => {
+	console.log(process.pid + " handling: " + request.url);
 	let path = parse(request.url).pathname;
 	if (path === "/client.bundle.js") {
 		fs.readFile("./dist/client.bundle.js", (error, data) => {
@@ -40,3 +53,4 @@ var port = process.env.PORT || 3000;
 server.listen(port, () => {
 	console.log("Server listening on port " + port);
 });
+}
