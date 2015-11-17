59be24515c2b34d37e26b47646f311aeff47ef20
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-16 22:28:03 -0800

    Updated location page

--- src/js/components/location.jsx
+++ src/js/components/location.jsx
@@ -11,7 +11,7 @@ let imageStyle = {
 	borderRadius: "50%",
 	borderStyle: "solid",
 	borderWidth: 2,
-	maxWidth: 240,
+	maxWidth: 320,
 	width: "100%"
 };
 
@@ -32,11 +32,8 @@ let phoneStyle = makeSectionStyle(phoneSrc);
 let gridStyle = {
 	width: "100%",
 	maxWidth: 240,
-	display: "flex",
-	flexWrap: "wrap",
 	float: "right",
 	marginTop: 20,
-	justifyContent: "space-between",
 	height: "0%"
 };
 
@@ -45,60 +42,59 @@ let gridImgStyle = {
 	display: "inline-block",
 	height: 70,
 	marginBottom: 20,
-	width: "calc(50% - 15px)"
+	width: "calc(50% - 15px)",
 };
-
-let images = [
-	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn2_1.jpg",
-	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn1_1.jpg",
-	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn3_1.jpg",
-	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn5.jpg"
-];
-let imageIndex = 0;
 
 class Location extends React.Component {
 	static displayName = "Location";
-	constructor(props) {
-		super(props);
-		this.state = { imageIndex: 0 };
-	}
-
-	updateImage(event) {
-		this.setState({ imageIndex: images.indexOf(event.target.src) })
-	}
 
 	render() {
 		return (
-			<div>
-			<div className="col-sm-6" style={{ textAlign: "right", paddingTop: 20 }}>
-				<img style={imageStyle} src={images[this.state.imageIndex]} />
-				<div className="image-carousel" style={gridStyle}>
-					{images.map((src) => <img onClick={this.updateImage.bind(this)} src={src} style={gridImgStyle} />)}
+			<div className="row location">
+				<div className="col-sm-6 images">
+					<img style={imageStyle} src="http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn2_1.jpg" />
 				</div>
-			</div>
-			<div className="col-sm-6">
-				<h1 style={{ fontFamily: "Dancing Script" }}>Temecula Creek Inn</h1>
-				<p style={websiteStyle}>
-					<a href="http://www.temeculacreekinn.com/" target="_blank">
-						Visit Website
-					</a>
-				</p>
-				<p style={phoneStyle}>
-					<a href="tel:8556856299" target="_blank">
-						(855) 685&ndash;6299
-					</a>
-				</p>
-				<p style={locationStyle}>
-					44501 Rainbow Canyon Rd<br/>
-					Temecula, CA 92592<br/>
-					<a className="btn btn-primary" role="button" href="https://goo.gl/maps/vtbPUY13pA52" target="_blank"
-						style={{
-							marginTop: 10
-						}}>
-						View in Google Maps
-					</a>
-				</p>
-			</div>
+				<div className="col-sm-6 info">
+					<h1 style={{ fontFamily: "Dancing Script" }}>Temecula Creek Inn</h1>
+					<div className="info-detail">
+						<p style={websiteStyle}>
+							<a href="http://www.temeculacreekinn.com/" target="_blank">
+								Visit Website
+							</a>
+						</p>
+						<p style={phoneStyle}>
+							<a href="tel:8556856299" target="_blank">
+								(855) 685&ndash;6299
+							</a>
+						</p>
+						<p style={locationStyle}>
+							44501 Rainbow Canyon Rd<br/>
+							Temecula, CA 92592<br/>
+							<a className="btn btn-primary" role="button" href="https://goo.gl/maps/vtbPUY13pA52" target="_blank"
+								style={{
+									marginTop: 10
+								}}>
+								View in Google Maps
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="col-xs-12 travel-advisory">
+					<div className="travel-advisory-inner">
+					<p>Ligula. Nunc turpis. Mauris vitae sapien. Nunc luctus bibendum velit.</p>
+
+					<p>Morbi faucibus volutpat sapien. Nam ac mauris at justo adipiscing facilisis.
+					Nunc et velit. Donec auctor, nulla id laoreet volutpat, pede erat feugiat ante,
+					auctor facilisis dui augue non turpis. Suspendisse mattis metus et justo.
+					Aliquam erat volutpat. Suspendisse potenti. Nam hendrerit lorem commodo metus
+					laoreet ullamcorper. Proin vel nunc a felis sollicitudin pretium. Maecenas in
+					metus at mi mollis posuere. Quisque ac quam sed massa adipiscing rutrum.
+					Vestibulum ipsum. Phasellus porta sapien. Maecenas venenatis tellus vel tellus.</p>
+
+					<p>Aliquam aliquam dolor at justo. Cum sociis natoque penatibus et magnis dis
+					parturient montes, nascetur.</p>
+				</div>
+				</div>
 			</div>
 		)
 	}
