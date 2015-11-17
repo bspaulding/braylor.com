9654a03037b7fc510d934e7c044fb85f7412a447
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-16 17:15:25 -0800

    Updated location page

--- src/js/components/location.jsx
+++ src/js/components/location.jsx
@@ -1,6 +1,9 @@
 import Colors from "../colors.js";
 import React from "react";
 import Banner from "./banner.jsx";
+import locationPinSrc from "../../images/location-pin.svg"
+import shareSrc from "../../images/share.svg"
+import phoneSrc from "../../images/phone.svg"
 
 let imageStyle = {
 	boxShadow: "0px 1px 6px #000",
@@ -8,30 +11,93 @@ let imageStyle = {
 	borderRadius: "50%",
 	borderStyle: "solid",
 	borderWidth: 2,
-	width: 240
+	maxWidth: 240,
+	width: "100%"
+};
+
+function makeSectionStyle(src) {
+	return {
+		backgroundImage: `url(${src})`,
+		backgroundPosition: "3px 0px",
+		backgroundRepeat: "no-repeat",
+		backgroundSize: 18,
+		paddingLeft: 34,
+		minHeight: 34
+	};
+}
+let locationStyle = makeSectionStyle(locationPinSrc);
+let websiteStyle = makeSectionStyle(shareSrc);
+let phoneStyle = makeSectionStyle(phoneSrc);
+
+let gridStyle = {
+	width: "100%",
+	maxWidth: 240,
+	display: "flex",
+	flexWrap: "wrap",
+	float: "right",
+	marginTop: 20,
+	justifyContent: "space-between",
+	height: "0%"
+};
+
+let gridImgStyle = {
+	cursor: "pointer",
+	display: "inline-block",
+	height: 70,
+	marginBottom: 20,
+	width: "calc(50% - 15px)"
 };
 
 let images = [
-	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn1_1.jpg",
 	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn2_1.jpg",
+	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn1_1.jpg",
 	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn3_1.jpg",
 	"http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn5.jpg"
-]
+];
+let imageIndex = 0;
 
 class Location extends React.Component {
 	static displayName = "Location";
+	constructor(props) {
+		super(props);
+		this.state = { imageIndex: 0 };
+	}
+
+	updateImage(event) {
+		this.setState({ imageIndex: images.indexOf(event.target.src) })
+	}
 
 	render() {
 		return (
 			<div>
-			<div className="col-xs-12">
-				<Banner>Location</Banner>
+			<div className="col-sm-6" style={{ textAlign: "right", paddingTop: 20 }}>
+				<img style={imageStyle} src={images[this.state.imageIndex]} />
+				<div className="image-carousel" style={gridStyle}>
+					{images.map((src) => <img onClick={this.updateImage.bind(this)} src={src} style={gridImgStyle} />)}
+				</div>
 			</div>
 			<div className="col-sm-6">
-				<img style={imageStyle} src="http://www.herecomestheguide.com/images/area_images/TemeculaCreekInn2_1.jpg"/>
-			</div>
-			<div className="col-sm-6">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.2971746295693!2d-117.13137148481586!3d33.46761295575872!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80db802b77b2d255%3A0x2f1ee894d2f0c8fd!2sTemecula+Creek+Inn!5e0!3m2!1sen!2sus!4v1447631753167" width="400" height="300" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+				<h1 style={{ fontFamily: "Dancing Script" }}>Temecula Creek Inn</h1>
+				<p style={websiteStyle}>
+					<a href="http://www.temeculacreekinn.com/" target="_blank">
+						Visit Website
+					</a>
+				</p>
+				<p style={phoneStyle}>
+					<a href="http://www.temeculacreekinn.com/" target="_blank">
+						(855) 685&ndash;6299
+					</a>
+				</p>
+				<p style={locationStyle}>
+					44501 Rainbow Canyon Rd<br/>
+					Temecula, CA 92592<br/>
+					<a className="btn btn-primary" role="button" href="https://goo.gl/maps/vtbPUY13pA52" target="_blank"
+						style={{
+							marginTop: 10
+						}}>
+						View in Google Maps
+					</a>
+				</p>
 			</div>
 			</div>
 		)
