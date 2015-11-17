fe65594880d6039cd75d835bca823fa9f11359b9
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-17 10:56:35 -0800

    Added some WIP copy to menu page

--- src/js/components/menu.jsx
+++ src/js/components/menu.jsx
@@ -5,7 +5,32 @@ class Menu extends React.Component {
 	static displayName = "Menu";
 
 	render() {
-		return <Banner>Menu</Banner>;
+		return (
+			<div className="row menu">
+				<div className="col-xs-12">
+					<Banner>Starters</Banner>
+					<dl>
+						<dt>fire roasted tomato and fennel bisque</dt>
+						<dd>arugula pesto</dd>
+						<dt>poached pear salad</dt>
+						<dd>spoon leaf spinach, candied walnuts, gorgonzola cheese & seasonal tomatoes</dd>
+					</dl>
+
+					<Banner>Entrees</Banner>
+					<dl>
+						<dt>four cheese ravioli</dt>
+						<dd>arugula pesto fillet of roasted tomato &amp; shaved squash</dd>
+						<dt>pan seared breast of chicken</dt>
+						<dd>peppercorn, port &amp; medeira sauce</dd>
+						<dt>TODO</dt>
+						<dd>Get the ribs title and description</dd>
+					</dl>
+
+					<Banner>Sides</Banner>
+					<p>TODO: do these need to be paired with dishes? how many do we get? what did we select?</p>
+				</div>
+			</div>
+		)
 	}
 }
 
