fed28f384eaa2e5cdaa08bda280da6446dffbbd4
Bradley Spaulding <brad.spaulding@gmail.com>
2015-11-19 22:28:14 -0800

    Persisting state on client with redux-localstorage

--- src/js/store.js
+++ src/js/store.js
@@ -1,27 +1,29 @@
 import { routerStateReducer } from 'redux-router';
 import {
-  combineReducers,
-  applyMiddleware,
-  compose,
-  createStore
+	combineReducers,
+	applyMiddleware,
+	compose,
+	createStore
 } from 'redux';
+import persistState from 'redux-localstorage';
 import createLogger from "redux-logger";
 import photosReducer from "./reducers/photos_reducer.js";
 import userReducer from "./reducers/user_reducer.js";
 import routes from "./components/routes.jsx";
 
 const reducer = combineReducers({
-  router: routerStateReducer,
-  photos: photosReducer,
+	router: routerStateReducer,
+	photos: photosReducer,
 	user: userReducer
 });
 
 export function makeStore(reduxReactRouter, createHistory, initialState) {
-  return compose(
-    // devTools()
-    reduxReactRouter({
-      routes,
-      createHistory
-    })
-  )(createStore)(reducer, initialState);
+	return compose(
+		// devTools()
+		persistState(['photos', 'user']),
+		reduxReactRouter({
+			routes,
+			createHistory
+		})
+	)(createStore)(reducer, initialState);
 }
