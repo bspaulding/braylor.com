f4566a01f2aae327af3706b27d7ce68a2b4961c8
Bradley J. Spaulding <brad.spaulding@gmail.com>
2018-07-29 20:59:37 -0700

    Hardcoded cloudfront urls to correct videos (git lfs + webpack => wrong hash)

--- src/js/components/video.jsx
+++ src/js/components/video.jsx
@@ -15,10 +15,10 @@ class Video extends Component {
 					controls
 					style={{ marginBottom: '20px', width: '100%' }}>
 					<source src='https://dl3qhh2xbcrs2.cloudfront.net/wedding-highlight-hls/prog_index.m3u8'/>
-					<source src={mp4High} type='video/mp4'/>
-					<source src={webm} type='video/webm'/>
-					<source src={ogg} type='video/ogg'/>
-					<source src={mp4Low} type='video/mp4'/>
+					<source src="https://dl3qhh2xbcrs2.cloudfront.net/205d6ce23c24b63c13e15b57c063e79c.mp4" type='video/mp4'/>
+					<source src="https://dl3qhh2xbcrs2.cloudfront.net/0969aead51c61eb3380bfe105ca7b337.webm" type='video/webm'/>
+					<source src="https://dl3qhh2xbcrs2.cloudfront.net/ff42c5f9ad07427dd721fd19df9128dd.ogg" type='video/ogg'/>
+					<source src="https://dl3qhh2xbcrs2.cloudfront.net/df0d915793000733fc0bbdf35245a63b.mp4" type='video/mp4'/>
 				</video>
 				<span>
 					Created by <a href="http://anthologyfilms.com/" target="_blank">Anthology Films</a>
