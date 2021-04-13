bb3618799edf133473b06ee4dba86f5e4f36d4d0
Bradley Spaulding <brad.spaulding@gmail.com>
2021-04-13 14:09:45 -0700

    Added webm video source

--- src/js/components/video.jsx
+++ src/js/components/video.jsx
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
 const mediaHost = 'http://media.braylor.com.s3-website-us-west-2.amazonaws.com';
 const mp4Low = `${mediaHost}/wedding-highlight-low.mp4`;
 const ogg = `${mediaHost}/wedding-highlight.ogg`;
+const webm = `${mediaHost}/wedding-highlight.webm`;
 
 class Video extends Component {
 	static displayName = "Video";
@@ -19,6 +20,7 @@ class Video extends Component {
 					controls
 					style={{ marginBottom: '20px', width: '100%' }}>
 					<source src={mp4Low} type='video/mp4'/>
+					<source src={webm} type='video/webm'/>
 					<source src={ogg} type='video/ogg'/>
 				</video>
 				<span>
