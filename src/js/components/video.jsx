d2459878a481a930e17050e1455c6b80be42221b
Bradley Spaulding <brad.spaulding@gmail.com>
2021-04-13 11:04:34 -0700

    Added ogg video back

--- src/js/components/video.jsx
+++ src/js/components/video.jsx
@@ -6,6 +6,7 @@ import React, { Component } from 'react';
 
 const mediaHost = 'http://media.braylor.com.s3-website-us-west-2.amazonaws.com';
 const mp4Low = `${mediaHost}/wedding-highlight-low.mp4`;
+const ogg = `${mediaHost}/wedding-highlight.ogg`;
 
 class Video extends Component {
 	static displayName = "Video";
@@ -18,6 +19,7 @@ class Video extends Component {
 					controls
 					style={{ marginBottom: '20px', width: '100%' }}>
 					<source src={mp4Low} type='video/mp4'/>
+					<source src={ogg} type='video/ogg'/>
 				</video>
 				<span>
 					Created by <a href="http://anthologyfilms.com/" target="_blank">Anthology Films</a>
